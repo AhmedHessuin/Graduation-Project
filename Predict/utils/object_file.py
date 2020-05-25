f1cc154bbe55070cf723c0dd6653d2e5d2fc1aa4
AhmedHessuin <36261222+AhmedHessuin@users.noreply.github.com>
2020-05-25 14:10:55 +0200

    changing object_file.py
    adding hand_written, safe_mode, original_image and replace curved arrow with incline arrow

--- Predict/utils/object_file.py
+++ Predict/utils/object_file.py
@@ -7,13 +7,16 @@ from utils import data_class as DC
 #======================================================================================================================#
 all_objects=[]
 object_id=0
-all_objects_as_dic={"/":[],"0":[],"1":[],"2":[],"3":[],"4":[],"5":[],"6":[],"7":[],"8":[],"9":[],"arrow head":[],"state condition":[],"loop back arrow":[],"state":[],"straight arrow":[],"curved arrow":[]}
+all_objects_as_dic={"/":[],"0":[],"1":[],"2":[],"3":[],"4":[],"5":[],"6":[],"7":[],"8":[],"9":[],"arrow head":[],"state condition":[],"loop back arrow":[],"state":[],"straight arrow":[],"incline arrow":[]}
 transaction=[]
 undo_all_objects=[]
+original_image=0
+safe_mode=1
 image=0
 image_copy=0
 image_test=0
 valid_verilog=False
+hand_written=False
 output_path=""
 image_name=""
 input_path=""
@@ -25,6 +28,7 @@ model_4=""
 model_5=""
 anchorDialogResult = ""
 anchorDialogChoice = ""
+
 #======================================================================================================================#
 
 
